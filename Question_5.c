8b4f85e21ffc1c50ca198f7078a1a29b90f36be1
Rabin chhatkuli <140541319+Rabinxt@users.noreply.github.com>
2024-02-28 22:24:43 +0545

    Update Question_5.c

--- Question_5.c
+++ Question_5.c
@@ -1,2 +1,23 @@
 //5. Create a function to reverse a given integer in C.
 
+#include<stdio.h>
+
+int reverse(int number) {
+    int rev = 0;
+    while (number != 0) {
+        rev = rev * 10 + number % 10;
+        number = number / 10;
+    }
+    return rev;
+}
+
+int main() {
+    int number;
+    printf("Enter the number: ");
+    scanf("%d", &number);
+
+    int reversed = reverse(number);
+    printf("Reversed number: %d\n", reversed);
+
+    return 0;
+}
